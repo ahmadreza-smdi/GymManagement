890e67318590e178fa875674aefa872e84e3a03c
ahmadreza <ahmadreza.smdi@gmail.com>
2018-07-31 15:53:16 +0430

    Feature:#3 Create login,register page

---
+++ Gym/web_athlete/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$',views.main,name="home"),
+    url(r'^login/$',views.login),
+    url(r'^register/$',views.register),
+]
--- Gym/web_athlete/views.py
+++ Gym/web_athlete/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect
 from . import models
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 # Create your views here.
+
+
+def main(request):
+    if request.method == 'GET':
+        pass
+    return render(request,'index.html')
+
+
+
+def login(request):
+    if request.method == 'POST':
+        username=request.POST.get('username','')
+        print("username:",username)
+        password=request.POST.get("password",'')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+           return HttpResponseRedirect('/register')
+        else:
+            return HttpResponseRedirect('/login/')
+
+    return render(request,'Signin.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username','')
+        password = request.POST.get('password','')
+        email = request.POST.get('email','')  
+        new_save=User(username=username,password=password,email=email)
+        new_save.save()
+        return HttpResponseRedirect('/login/')
+       
+    return render(request,'Signup.html')
