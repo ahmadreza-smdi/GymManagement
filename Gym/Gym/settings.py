281253fc60592bed15e13a795ebdd3c5978c4a89
Ahmadreza <ahmadreza.smdi@gmail.com>
2018-08-14 13:03:46 +0430

    clean the help branch tree

--- Gym/Gym/settings.py
+++ Gym/Gym/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 
-            ],
+                ],
         },
     },
 ]
